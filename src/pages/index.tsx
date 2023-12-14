5f03421d1dda38dfa5ae7e4885524d3fbbb99a01
Ilhamnrachman <noerr.ilham@gmail.com>
2023-12-14 09:30:57 +0700

    add: icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
                     name="viewport"
                     content="width=device-width, initial-scale=1"
                 />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/simple-quicks.svg" />
             </Head>
             <main className={`${lato.className}`}>
                 <div className="side-main"></div>
