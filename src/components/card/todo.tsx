dd917bd9a5caa1120e4bff643b4466d92f7271c8
Ilhamnrachman <noerr.ilham@gmail.com>
2024-01-04 01:48:08 +0700

    opts

--- src/components/card/todo.tsx
+++ src/components/card/todo.tsx
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 
 // antd
-import { Collapse, Checkbox, Space, DatePicker } from 'antd';
+import {
+    Collapse,
+    Checkbox,
+    Space,
+    DatePicker,
+    Popover,
+    Select,
+    Tag,
+} from 'antd';
 // import type { CheckboxValueType } from 'antd/es/checkbox/Group';
 import type { CheckboxChangeEvent } from 'antd/es/checkbox';
 // components
@@ -10,6 +18,75 @@ import Loading from '@/Components/loading';
 // type
 import type { NotifData } from '@/Types/msg.type';
 import Image from 'next/image';
+import { EllipsisOutlined } from '@ant-design/icons';
+import type { CustomTagProps } from 'rc-select/lib/BaseSelect';
+
+// const opts = () => {
+//     const dt = [
+//         {
+//             label: ' Important ASAP',
+//             value: '#E5F1FF',
+//         },
+//         { label: 'Office Meeting', value: '#FDCFA4' },
+//         { label: 'Virtuan Meeting', value: '#F9E9C3' },
+//         { label: 'ASAP', value: '#AFEBDB' },
+//         { label: 'Client Related', value: '#CBF1C2' },
+//         { label: 'Self Task', value: '#CFCEF9' },
+//         { label: 'Appoinment', value: '#F9E0FD' },
+//         { label: 'Court Related', value: '#9DD0ED' },
+//     ];
+//     const result = [];
+//     result.push(
+//         dt.map((data) => ({
+//             ...data,
+//             label: (
+//                 <div
+//                     style={{
+//                         background: data.value,
+//                         border: '1px solid black',
+//                         borderRadius: '3px',
+//                     }}
+//                 >
+//                     {data.label}
+//                 </div>
+//             ),
+//         })),
+//     );
+
+//     return result[0];
+// };
+
+const options = [
+    {
+        label: 'Important ASAP',
+        value: '#E5F1FF',
+    },
+    { label: 'Office Meeting', value: '#FDCFA4' },
+    { label: 'Virtuan Meeting', value: '#F9E9C3' },
+    { label: 'ASAP', value: '#AFEBDB' },
+    { label: 'Client Related', value: '#CBF1C2' },
+    { label: 'Self Task', value: '#CFCEF9' },
+    { label: 'Appoinment', value: '#F9E0FD' },
+    { label: 'Court Related', value: '#9DD0ED' },
+];
+const tagRender = (props: CustomTagProps) => {
+    const { label, value, closable, onClose } = props;
+    const onPreventMouseDown = (event: React.MouseEvent<HTMLSpanElement>) => {
+        event.preventDefault();
+        event.stopPropagation();
+    };
+    return (
+        <Tag
+            color={value}
+            onMouseDown={onPreventMouseDown}
+            closable={closable}
+            onClose={onClose}
+            style={{ marginRight: 3, color: 'black' }}
+        >
+            <strong>{label}</strong>
+        </Tag>
+    );
+};
 
 const MainTodo: React.FC<{
     dataNotif: NotifData[];
@@ -22,6 +99,7 @@ const MainTodo: React.FC<{
         Array.isArray(params)
             ? params.map((data, index: number) => ({
                   key: data.id,
+
                   label: (
                       <>
                           <div className="row">
@@ -97,11 +175,43 @@ const MainTodo: React.FC<{
                                           }
                                           defaultValue={data.body}
                                           onChange={(e) => e.target.value}
+                                          placeholder="No Description"
                                       />
                                   </div>
                               </Space>
+                              <div className="mock-block">
+                                  <Select
+                                      mode="multiple"
+                                      placeholder="Please select"
+                                      style={{ width: '90%' }}
+                                      options={options}
+                                      tagRender={tagRender}
+                                  />
+                              </div>
                           </Space>
                       </div>
+                  ),
+                  extra: (
+                      <Popover
+                          className="delete"
+                          style={{
+                              border: ' 1px solid #828282',
+                              color: 'yellow',
+                          }}
+                          placement="bottom"
+                          trigger="click"
+                          content={
+                              <div
+                                  style={{
+                                      cursor: 'pointer',
+                                  }}
+                              >
+                                  <div className="indicators-2">Delete</div>
+                              </div>
+                          }
+                      >
+                          <EllipsisOutlined className="ml-sm" />
+                      </Popover>
                   ),
               }))
             : [];
@@ -109,12 +219,23 @@ const MainTodo: React.FC<{
     return props.isLoading ? (
         <Loading />
     ) : (
-        <Collapse
-            style={{ backgroundColor: 'white' }}
-            bordered={false}
-            items={items(props.dataNotif)}
-            expandIconPosition="end"
-        />
+        <>
+            <Collapse
+                collapsible="icon"
+                style={{ backgroundColor: 'white' }}
+                bordered={false}
+                items={items(props.dataNotif)}
+                expandIconPosition="end"
+            />
+            <style>
+                {`
+                    .ant-popover-content > .ant-popover-inner {
+                        border: 1px solid #828282;
+                        
+                    }
+                `}
+            </style>
+        </>
     );
 };
 
